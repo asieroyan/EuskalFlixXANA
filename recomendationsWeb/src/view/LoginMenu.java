8c75097c40ac39e1ecb3243b0fff93cc3904f8fb
Nico150918 <41684208+Nico150918@users.noreply.github.com>
2019-05-12 16:22:02 +0200

    Las ventanas hechas y funcionando

--- recomendationsWeb/src/view/LoginMenu.java
+++ recomendationsWeb/src/view/LoginMenu.java
@@ -6,101 +6,88 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
+
+import packEuskoFlix.RatingCatalogue;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JRadioButton;
 import javax.swing.JCheckBox;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class LoginMenu extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtIntroduceYourId;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginMenu frame = new LoginMenu();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private JTextField textField;
+	
 	/**
 	 * Create the frame.
 	 */
 	public LoginMenu() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1300, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("default:grow"),}));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblEuskoflix = new JLabel("EUSKOFLIX");
-		lblEuskoflix.setFont(new Font("Dialog", Font.BOLD, 20));
-		contentPane.add(lblEuskoflix, "4, 2, 3, 1");
-		
-		JButton btnEnglish = new JButton("Settings");
-		contentPane.add(btnEnglish, "6, 4");
-		
-		JLabel lblIntroduceYourId = new JLabel("Introduce your ID");
-		contentPane.add(lblIntroduceYourId, "4, 10");
-		
-		txtIntroduceYourId = new JTextField();
-		txtIntroduceYourId.setToolTipText("Introduce your ID");
-		contentPane.add(txtIntroduceYourId, "4, 12, 3, 1, fill, default");
-		txtIntroduceYourId.setColumns(10);
-		
-		JButton btnSubmit = new JButton("Submit");
-		contentPane.add(btnSubmit, "6, 14");
 		JPanel panel = new JPanel();
-		contentPane.add(panel, "4, 18, 3, 1, fill, fill");
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(395, 153, 295, 26);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblUserId = new JLabel("User Id");
+		lblUserId.setBounds(329, 156, 69, 20);
+		panel.add(lblUserId);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setBounds(395, 237, 395, 217);
+		panel.add(lblNewLabel);
+		
+		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				MainMenu menu = (MainMenu) getFrames()[1];				
+				if (textField.getText()!=null && RatingCatalogue.getRatingCatalogue().getAllUsers().contains(Integer.parseInt(textField.getText()))) {
+					menu.setVisible(true);
+					getFrames()[0].setVisible(false);
+					menu.setUser(textField.getText());
+					lblNewLabel.setText("");
+				}else {
+					textField.setText("");
+					lblNewLabel.setText("Invalid user Id try again");
+				};				
+				
+			}
+		});
+		
+		btnLogIn.setBounds(733, 139, 149, 50);
+		panel.add(btnLogIn);
+		
+		JLabel lblNewLabel_1 = new JLabel(new ImageIcon("Logo.png"));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		contentPane.add(lblNewLabel_1, BorderLayout.NORTH);
+		
+
 		
 		javax.swing.ImageIcon Imagen = new javax.swing.ImageIcon("/home/lsi/Descargas/a.png");
-		javax.swing.JLabel Img = new javax.swing.JLabel(Imagen);
-		Img.setSize(120, 160);
-		panel.add(Img);
-		
 		
 		
 	}
-
 }
--- recomendationsWeb/src/view/MainMenu.java
+++ recomendationsWeb/src/view/MainMenu.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 
 import packEuskoFlix.FilmCatalogue;
 import packEuskoFlix.RatingCatalogue;
+import packEuskoFlix.VectorInteger;
 
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -25,43 +26,41 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 
-public class MainMenu extends JFrame implements Observer {
+public class MainMenu extends JFrame {
 
 	private JPanel CentralPanel;
-	private final JLabel lblEuskoflix = new JLabel("EUSKOFLIX");
+	private final JLabel lblEuskoflix = new JLabel(new ImageIcon("Logo.png"));
 	private JTextField UserIDText;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainMenu frame = new MainMenu();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
 	public MainMenu() {
+		setResizable(false);
 		FilmCatalogue filmCatalogue = FilmCatalogue.getFilmCatalogue();
 		filmCatalogue.initializeTitles();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1300, 800);
 		CentralPanel = new JPanel();
 		CentralPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(CentralPanel);
 		CentralPanel.setLayout(new BorderLayout(0, 0));
+		lblEuskoflix.setBackground(Color.BLACK);
+		lblEuskoflix.setFont(new Font("Dialog", Font.BOLD, 24));
+		CentralPanel.add(lblEuskoflix, BorderLayout.NORTH);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
 		CentralPanel.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new BorderLayout(0, 0));
 		
@@ -72,13 +71,15 @@ public class MainMenu extends JFrame implements Observer {
 		panel_1.add(UserID);
 		
 		UserIDText = new JTextField();
+		UserIDText.setEditable(false);
 		panel_1.add(UserIDText);
 		UserIDText.setColumns(10);
 		
 		JButton btnRecommend = new JButton("RECOMMEND");
 		panel_1.add(btnRecommend);
 		
-		JButton btnNormalized = new JButton("Normalized");
+
+		JButton btnNormalized = new JButton("Not normalized");
 		btnNormalized.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_1.add(btnNormalized);
 		
@@ -91,8 +92,21 @@ public class MainMenu extends JFrame implements Observer {
 		panel_3.setLayout(new BorderLayout(0, 0));
 		
 		JComboBox ValorationMode = new JComboBox();
+		ValorationMode.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		ValorationMode.setToolTipText("Select the filter mode, then press change.");
 		panel_3.add(ValorationMode, BorderLayout.CENTER);
+		ValorationMode.addItem("Filter by user");
+		ValorationMode.addItem("Filter by content");
+		ValorationMode.addItem("Filter by product");
 		
+		JButton btnNewButton = new JButton("Change");
+		btnNewButton.setToolTipText("Press it to change the filter type");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectFilterType((String) ValorationMode.getSelectedItem());
+			}
+		});
+		panel_3.add(btnNewButton, BorderLayout.EAST);
 		JPanel panel_4 = new JPanel();
 		panel_2.add(panel_4, BorderLayout.CENTER);
 		panel_4.setLayout(new BorderLayout(0, 0));
@@ -100,46 +114,115 @@ public class MainMenu extends JFrame implements Observer {
 		JScrollPane scrollPane = new JScrollPane();
 		panel_4.add(scrollPane, BorderLayout.CENTER);
 		
-		JList list = new JList();
-		scrollPane.setViewportView(list);
-		
-		JList list_1 = new JList();
-		scrollPane.setRowHeaderView(list_1);
+		JTextArea FilmsTextArea = new JTextArea();
+		FilmsTextArea.setFont(new Font("Arial", Font.BOLD, 16));
+		FilmsTextArea.setForeground(Color.WHITE);
+		FilmsTextArea.setBackground(Color.DARK_GRAY);
+		scrollPane.setViewportView(FilmsTextArea);
+		FilmsTextArea.setEditable(false);
 		
 		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Color.LIGHT_GRAY);
 		panel_4.add(panel_5, BorderLayout.WEST);
-		panel_5.setLayout(new GridLayout(3, 1, 0, 2));
+		GridBagLayout gbl_panel_5 = new GridBagLayout();
+		gbl_panel_5.columnWidths = new int[] {113};
+		gbl_panel_5.rowHeights = new int[] {0, 30, 30, 0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
+		gbl_panel_5.columnWeights = new double[]{0.0};
+		gbl_panel_5.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		panel_5.setLayout(gbl_panel_5);
 		
-		JButton btnNewButton = new JButton("Show Films");
-		panel_5.add(btnNewButton);
+		JButton exit = new JButton("Exit");
+		exit.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		
-		JButton btnNewButton_1 = new JButton("Settings");
-		panel_5.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("Exit");
-		panel_5.add(btnNewButton_2);
+		JButton showFilms = new JButton("Show Films");
+		showFilms.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		GridBagConstraints gbc_showFilms = new GridBagConstraints();
+		gbc_showFilms.insets = new Insets(0, 0, 5, 0);
+		gbc_showFilms.gridx = 0;
+		gbc_showFilms.gridy = 0;
+		panel_5.add(showFilms, gbc_showFilms);
+		showFilms.setToolTipText("");
+		
+		JButton btnChangeUser = new JButton("Change User");
+		btnChangeUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {				
+				getFrames()[0].setVisible(true);
+				getFrames()[1].setVisible(false);
+				
+			}
+		});
+		
+		GridBagConstraints gbc_btnChangeUser = new GridBagConstraints();
+		gbc_btnChangeUser.fill = GridBagConstraints.VERTICAL;
+		gbc_btnChangeUser.insets = new Insets(0, 0, 5, 0);
+		gbc_btnChangeUser.gridx = 0;
+		gbc_btnChangeUser.gridy = 2;
+		panel_5.add(btnChangeUser, gbc_btnChangeUser);
+		GridBagConstraints gbc_exit = new GridBagConstraints();
+		gbc_exit.gridx = 0;
+		gbc_exit.gridy = 16;
+		panel_5.add(exit, gbc_exit);
 		
 		JLabel lblFilms = new JLabel("Films");
 		lblFilms.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_4.add(lblFilms, BorderLayout.NORTH);
-		lblEuskoflix.setFont(new Font("Dialog", Font.BOLD, 20));
-		CentralPanel.add(lblEuskoflix, BorderLayout.NORTH);
 		
 		//Button RecommendFilm
 		btnRecommend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//chckbxNormalized.get
-				//RatingCatalogue.getRatingCatalogue().changeNormalizeMode(); //cambia el modo
-				RatingCatalogue.getRatingCatalogue().recommendFilm(Integer.parseInt(UserIDText.getText()));
+			public void actionPerformed(ActionEvent arg0) {	
+				RatingCatalogue ratings=RatingCatalogue.getRatingCatalogue();
+				VectorInteger recommnededFilmTittles = RatingCatalogue.getRatingCatalogue().recommendFilm(Integer.parseInt(UserIDText.getText()));
+				String recommnededFilmTittlesText = recommnededFilmTittles.obtainFilmTittlesFromVector();
+				FilmsTextArea.setText(recommnededFilmTittlesText);
 			};
+		});
+		
+		btnNormalized.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (btnNormalized.getText().equals("Normalized")) {
+					btnNormalized.setText("Not normalized");
+				}else {
+					btnNormalized.setText("Normalized");
+				}
+				RatingCatalogue.getRatingCatalogue().changeNormalizeMode();
+			};
+		});
+		
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		showFilms.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String FilmTittlesText = filmCatalogue.getAllFilms().obtainFilmTittlesFromVector();
+				FilmsTextArea.setText(FilmTittlesText);
+				FilmsTextArea.setCaretPosition(0);
+			}
 		});
 	}
 	
+	public void setUser(String pUser) {
+		UserIDText.setText(pUser);
+		
+	}	
 	
-
-	@Override
-	public void update(Observable o, Object arg) {
+	private void selectFilterType(String pFilter) {
+		if (pFilter.equals("Filter by user")) {
+			System.out.println("User");
+			RatingCatalogue.getRatingCatalogue().changeValorationMode("UserFilter");
+		}else if(pFilter.equals("Filter by content")){
+			System.out.println("Content");
+			RatingCatalogue.getRatingCatalogue().changeValorationMode("ContentFilter");
+		}
+		else {
+			System.out.println("Product");
+			RatingCatalogue.getRatingCatalogue().changeValorationMode("ProductFilter");
+			
+		}
 		
 	}
+
 
 }
